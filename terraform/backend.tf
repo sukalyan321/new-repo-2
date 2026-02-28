eb4a66a59365446e6d27dd256b1e28195ecf5f64
Sukalyan <sukalyandebsingha@gmail.com>
2026-02-28 02:52:23 -0800

    add terraform code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "sukas-terraform-state-bucket"   # change this
+    bucket         = "sukas-terraform-state-bucket" # change this
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
--- terraform/main.tf
+++ terraform/main.tf
@@ -94,9 +94,9 @@ resource "aws_security_group" "web_sg" {
 # EC2 Instance
 # -------------------------
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
